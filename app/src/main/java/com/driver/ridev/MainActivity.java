3f410f22377e7b6c7803cb8d7fffb20384100cfe
danishAli07 <danishali245@gmail.com>
2020-03-15 16:24:32 +0530

    Added color and switch button

--- app/src/main/java/com/driver/ridev/MainActivity.java
+++ app/src/main/java/com/driver/ridev/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -20,10 +21,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private Switch mActiveSwitch;
+    private TextView mToolbarActiveText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +37,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -51,6 +49,43 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        initView();
+    }
+
+    private void initView() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        mActiveSwitch = (Switch)findViewById(R.id.switch_active);
+        mToolbarActiveText = findViewById(R.id.toolbar_title);
+        if (mActiveSwitch.isActivated()) {
+            mToolbarActiveText.setText("Online");
+            mToolbarActiveText.setTextColor(getResources().getColor(R.color.green_normal));
+        } else {
+            mToolbarActiveText.setText("Offline");
+            mToolbarActiveText.setTextColor(getResources().getColor(R.color.white));
+        }
+
+        mActiveSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Log.d("danny","switch value changes  value :"+b);
+                if (b){
+                    mToolbarActiveText.setText("Online");
+                    mToolbarActiveText.setTextColor(getResources().getColor(R.color.green_normal));
+                } else {
+                    mToolbarActiveText.setText("Offline");
+                    mToolbarActiveText.setTextColor(getResources().getColor(R.color.white));
+                }
+            }
+        });
+
     }
 
     @Override
